de66e9e91fd7605b88e6f21093a2548a596f5bb2
Patrick Schlaepfer <patrick@schlaepfer.com>
2015-04-23 15:52:16 +0200

    Added special treatment of Email for MS Outlook

---
+++ woocommerce/emails/email-header.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Email Header
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates/Emails
+ * @version     2.3.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title><?php echo get_bloginfo( 'name', 'display' ); ?></title>
+        <style>
+            @media screen {
+                @font-face {
+                    font-family: 'Lato';
+                    font-style: normal;
+                    font-weight: 400;
+                    src: local('Lato Regular'), local('Lato-Regular'), url(http://fonts.gstatic.com/s/lato/v11/1YwB1sO8YE1Lyjf12WNiUA.woff2) format('woff2');
+                    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;
+                }
+            }
+
+        </style>
+	</head>
+    <body <?php echo is_rtl() ? 'rightmargin' : 'leftmargin'; ?>="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
+    	<div id="wrapper">
+        	<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
+            	<tr>
+                	<td align="center" valign="top">
+						<div id="template_header_image">
+	                		<?php
+	                			if ( $img = get_option( 'woocommerce_email_header_image' ) ) {
+	                				echo '<p style="margin-top:0;"><img src="' . esc_url( $img ) . '" alt="' . get_bloginfo( 'name', 'display' ) . '" /></p>';
+	                			}
+	                		?>
+						</div>
+                    	<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_container">
+                        	<tr>
+                            	<td align="center" valign="top">
+                                    <!-- Header -->
+                                	<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_header">
+                                        <tr>
+                                            <td>
+                                            	<h1><?php echo $email_heading; ?></h1>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                    <!-- End Header -->
+                                </td>
+                            </tr>
+                        	<tr>
+                            	<td align="center" valign="top">
+                                    <!-- Body -->
+                                	<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_body">
+                                    	<tr>
+                                            <td valign="top" id="body_content">
+                                                <!-- Content -->
+                                                <table border="0" cellpadding="20" cellspacing="0" width="100%">
+                                                    <tr>
+                                                        <td valign="top">
+                                                            <div id="body_content_inner">
